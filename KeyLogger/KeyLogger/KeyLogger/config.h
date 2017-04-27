e3b71b7f0466746cc1eb5b8e362b80a73b792a6c
Carlos Martinez <ctmartinez1992@gmail.com>
2017-04-27 18:04:00 +0100

    commit

---
+++ KeyLogger/KeyLogger/KeyLogger/config.h
@@ -0,0 +1,12 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+
+#define PROJECT_NAME "Keylogger"
+#define PROJECT_VERSION_MAJOR 1
+#define PROJECT_VERSION_MINOR 1
+#define PROJECT_VERSION_PATCH 0
+#define PROJECT_COPYRIGHT "Keksimus Maximus"
+#define FILEEXT ".log"
+#define DEBUG 0
+
+#endif
